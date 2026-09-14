11606fe8ab75ed04496f848c7a1042d91e831f25
kailash <skailash27082k@gmail.com>
2026-09-14 15:52:52 +0530

    Add exmaples of different loops

---
+++ Loops/While.java
@@ -0,0 +1,10 @@
+package Loops;
+public class While {
+    public static void main(String[] args) {
+        int x=0;
+        while(x<10){
+            System.out.println("This is the "+(x+1)+" number");
+            x++;
+        }
+    }
+}
