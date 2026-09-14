6e3f6db8e6d46e35402316341edf62c737a64044
kailash <skailash27082k@gmail.com>
2026-09-14 15:29:59 +0530

    Ternary Operator example

---
+++ Conditionals/Ternary.java
@@ -0,0 +1,10 @@
+package Conditionals;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int x=50;
+        boolean result;
+        result = (x > 10) ? true : false;
+        System.out.println(result);
+    }   
+}
