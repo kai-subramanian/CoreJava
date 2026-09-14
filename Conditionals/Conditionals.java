73048b0e9d51838b8cb0f585d7b227e917996176
kailash <skailash27082k@gmail.com>
2026-09-14 15:26:12 +0530

    Adding conditionals

---
+++ Conditionals/Conditionals.java
@@ -0,0 +1,17 @@
+package Conditionals;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        int cost=10;
+        if(cost>=10){
+            System.out.println("Cost is greater than 10");
+        } else {
+            System.out.println("Cost is less than 10");
+        }
+
+        boolean x = true;
+        if(!x){
+            System.out.println("Hello");
+        }
+    }
+}
